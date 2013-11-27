b0b6924aad12c324dbd6ccb1329dbb088357dcb5
Tarabor <marco.taraborrelli@gmail.com>
2013-11-27 21:36:22 +0100

    Mappa con aggiornamenti ajax
    Adesso la mappa si aggiorna rispetto alle posizioni degli altri utenti e
    aggiorna la posizione del proprio utente sul server. In ajax.

--- assets/www/js/views/MapView.js
+++ assets/www/js/views/MapView.js
@@ -1,15 +1,18 @@
-define(["jquery", "underscore", "parse", "handlebars", "text!templates/map.html", "text!templates/marker-popup.html"], function($, _, Parse, Handlebars, template, popupTemplate) {
+define(["jquery", "underscore", "parse", "handlebars", "text!templates/map.html", "text!templates/marker-popup.html","models/Warrior"], function($, _, Parse, Handlebars, template, popupTemplate,Warrior) {
 
     var AdListView = Parse.View.extend({
 
         tagName: "div",
         id: "map",
-
+        model: Warrior,
+        self:undefined,
         template: Handlebars.compile(template),
         popupTemplate: Handlebars.compile(popupTemplate),
 
         initialize: function() {
             //this.model.on("reset", this.render, this);
+            self=this;
+            this.model=JSON.parse(window.localStorage.getItem("warrior"));
             this.on("inTheDom", this.addMap);
         },
 
@@ -46,15 +49,64 @@ define(["jquery", "underscore", "parse", "handlebars", "text!templates/map.html"
             }, function() {});
 
              var centromappa;
+             markers = new Array();
 
              function OnSuccess(position) {
-              //alert('Latitude: '          + position.coords.latitude          + '\n' +
-          //'Longitude: '         + position.coords.longitude         + '\n');
-              var a = new L.LatLng(position.coords.latitude, position.coords.longitude);
-              marker.setLatLng(a).update();
-              
-               map.setView( a,  17, {animate: true} );
+                 //alert('Latitude: ' + position.coords.latitude + '\n' +
+                 //'Longitude: ' + position.coords.longitude + '\n');
+                 var a = new L.LatLng(position.coords.latitude, position.coords.longitude);
+                 marker.setLatLng(a).update();
+
+                 map.setView(a, 17, {
+                     animate: true
+                 });
+                 self.model.position.latitude=position.coords.latitude;
+                 self.model.position.longitude=position.coords.longitude;
+                // aggiorniamo nostra posizione su parse
+                $.ajax({
+                type : 'PUT',
+                headers: {
+                'X-Parse-Application-Id' : "PHYN9yjJLqmSo0xACd63oypUBUcx4q0eLHBvoozY",
+                'X-Parse-REST-API-Key' : "VSn4lvHnF3EKZmVPox8rRA7Eol3X9r7M8blvQWfC"
+                },
+                url: "https://api.parse.com/1/classes/Warrior/"+self.model.objectId,
+                data:  JSON.stringify({"position" : self.model.position}),
+                contentType: "application/json; charset=utf-8",
+                dataType : "json"
+                });
+
+
+                 // visualizziamo altri warriors sulla mappa
+                 var point = new Parse.GeoPoint({
+                     latitude: position.coords.latitude,
+                     longitude: position.coords.longitude
+                 });
+
+                 var query = new Parse.Query(Warrior);
+                 // Interested in locations near user.
+                 query.near("position", point);
+                 // Limit what could be a lot of points.
+                 query.limit(10);
+                 // Final list of objects
+                 query.find({
+                     success: function (nearWarriors) {
+                         console.log(nearWarriors.length);
+                         for (i = 1; i < nearWarriors.length; i++) {
+                          console.log(nearWarriors[i].id); 
+                             if (markers[nearWarriors[i].id] == undefined)   {
+                             markers[nearWarriors[i].id] = L.marker([nearWarriors[i].get("position").latitude, nearWarriors[i].get("position").longitude]).addTo(map);
+                           }
+                             else {
+                              var b = new L.LatLng(nearWarriors[i].get("position").latitude, nearWarriors[i].get("position").longitude);
+                              markers[nearWarriors[i].id].setLatLng(b).update();
+                            }
+                         }
+                     }
+                 });
+
+
              };
+
 
              function onError(error) {
                alert('code: '    + error.code    + '\n' +
