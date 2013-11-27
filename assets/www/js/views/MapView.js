5729b51e4ce87c9b935f68f794294acd12cd03ed
Tarabor <marco.taraborrelli@gmail.com>
2013-11-27 10:28:31 +0100

    Tracking posizione e orientamento utente
    Mappa roteante!!

--- assets/www/js/views/MapView.js
+++ assets/www/js/views/MapView.js
@@ -20,10 +20,9 @@ define(["jquery", "underscore", "parse", "handlebars", "text!templates/map.html"
         },
 
         addMap: function() {
-            var options ={center: new L.LatLng(42.350711, 13.399961),
-              zoom: 13
-            };
-            var map = L.map('map', options);
+           
+            var map = new L.map('map', {center: new L.LatLng(42.350711, 13.399961),zoom: 17,zoomControl: false} );
+            //var googleLayer = new L.Google('ROADMAP');
 
             // add the markers about the ads
            /* for (var i = 0; i < this.model.length; i++) {
@@ -40,15 +39,61 @@ define(["jquery", "underscore", "parse", "handlebars", "text!templates/map.html"
               iconSize:     [20, 20], // size of the icon
             });
 
+            var marker;
+
             navigator.geolocation.getCurrentPosition(function (position) {
-              L.marker([position.coords.latitude, position.coords.longitude], {icon: positionIcon}).addTo(map);
+              marker = L.marker([position.coords.latitude, position.coords.longitude], {icon: positionIcon}).addTo(map);
             }, function() {});
 
+             var centromappa;
 
-            
+             function OnSuccess(position) {
+              //alert('Latitude: '          + position.coords.latitude          + '\n' +
+          //'Longitude: '         + position.coords.longitude         + '\n');
+              var a = new L.LatLng(position.coords.latitude, position.coords.longitude);
+              marker.setLatLng(a).update();
+              
+               map.setView( a,  17, {animate: true} );
+             };
 
-            L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {    
+             function onError(error) {
+               alert('code: '    + error.code    + '\n' +
+               'message: ' + error.message + '\n');
+          };
+
+
+
+
+            navigator.geolocation.watchPosition(OnSuccess, onError, { timeout: 2000 });
+
+            //var webkit =  document.getElementsByClassName("leaflet-marker-icon  leaflet-clickable leaflet-zoom-animated").style.webkitTransform;
+
+            function onCompassSuccess(heading) {
+                //alert( 'Heading: ' + heading.magneticHeading);
+                document.getElementById("map").style.webkitTransform = 'rotateZ('+heading.magneticHeading+'deg) rotateX(60deg)';
+                //document.getElementsByClassName("leaflet-marker-icon  leaflet-clickable leaflet-zoom-animated").style.webkitTransform = webkit + 'rotateZ('+heading.magneticHeading+'deg)';
+            };
+
+            function onCompassError(compassError) {
+                    alert('Compass error: ' + compassError.code);
+            };
+
+            var options = { frequency: 500 };  // Update every 3 seconds
+
+            var watchID = navigator.compass.watchHeading(onCompassSuccess, onCompassError, options);
+           
+
+
+            //map.addLayer(googleLayer);
+            L.tileLayer('http://{s}.tile.cloudmade.com/a6f26d33c61342318355eb8f812ce3ba/17306/256/{z}/{x}/{y}.png', {
+                
                 maxZoom: 20
+            }).addTo(map);
+
+            var circle = L.circle([42.350711,  13.399961], 50, {
+            color: 'blue',
+            fillColor: '#1F00FF',
+             fillOpacity: 0.5
             }).addTo(map);
 
             
