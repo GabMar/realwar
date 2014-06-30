bd80881ae38657ad439d18bec7a127657b7e3121
elle90 <lele200290@gmail.com>
2014-06-30 11:51:53 +0200

    Aggiunto spinner market

--- assets/www/js/views/WeaponsMarketView.js
+++ assets/www/js/views/WeaponsMarketView.js
@@ -22,14 +22,13 @@ define(["jquery", "underscore", "parse", "handlebars", "collections/WeaponsMarke
             this.model= new WeaponsMarketCollection();
             this.head= new HeadMarketCollection();
             this.armor= new ArmorMarketCollection();
-          this.model.bind("reset", this.render, this); //che fa?
+            this.model.bind("reset", this.render, this); //che fa?
          },
 
         weaponList: function () {
             $(this.el).empty();
             var ul = $(this.el).html(this.template);
-            $(".ball").hide();
-            $(".ball1").hide();
+            $("#spinner2").hide();
             _.each(this.model.models, function (weapon) {
                 $(ul).find('#weaponsmarket').append(new WeaponsMarketItemView({ //mi appende dentro this.el un elemento della lista
                     model: weapon
@@ -43,8 +42,7 @@ define(["jquery", "underscore", "parse", "handlebars", "collections/WeaponsMarke
             //ma io lo faccio così per ora
             $(this.el).empty();
             var ul = $(this.el).html(this.template);
-            $(".ball").hide();
-            $(".ball1").hide();
+            $("#spinner2").hide();
             $(ul).find('#btnHd').css("background-color", "#20A715");
             $(ul).find('#btnHd').css("color","#051E07");
             $(ul).find('#btnWpn').css("background-color", "rgb(5, 31, 7)");
@@ -64,8 +62,7 @@ define(["jquery", "underscore", "parse", "handlebars", "collections/WeaponsMarke
         armorList: function () {
             $(this.el).empty();
             var ul = $(this.el).html(this.template);
-            $(".ball").hide();
-            $(".ball1").hide();
+            $("#spinner2").hide();
             $(ul).find('#btnArm').css("background-color", "#20A715");
             $(ul).find('#btnArm').css("color","#051E07");
             $(ul).find('#btnWpn').css("background-color", "rgb(5, 31, 7)");
@@ -84,8 +81,7 @@ define(["jquery", "underscore", "parse", "handlebars", "collections/WeaponsMarke
         render: function (eventName) {
             $(this.el).empty();
             var ul = $(this.el).html(this.template);
-            $(".ball").hide();
-            $(".ball1").hide();
+            $("#spinner2").hide();
             _.each(this.model.models, function (weapon) {
                 $(ul).find('#weaponsmarket').append(new WeaponsMarketItemView({ //mi appende dentro this.el un elemento della lista
                     model: weapon
