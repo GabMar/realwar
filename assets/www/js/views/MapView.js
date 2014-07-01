7531ecbc30d70ad45a03d5324c6865c9d90c5510
mrsalx <andreasalx87@gmail.com>
2014-07-01 10:53:25 +0200

    Aggiunto il Circle al warrior nella mappa
    E' possibile combattere solamente con i warrior all'interno del proprio
    circle. Il raggio del circle viene calcolato in base al range di arma e
    head del warrior.

--- assets/www/js/views/MapView.js
+++ assets/www/js/views/MapView.js
@@ -28,13 +28,12 @@ function($, _, Parse, Handlebars, template, popupTemplate,Warrior, Weapon, Head,
         addMap: function() {
            
             var map = L.map('map', {center: new L.LatLng(42.350711, 13.399961),zoom: 17,zoomControl: false} );
-            
+
             L.tileLayer('http://{s}.tiles.mapbox.com/v3/tarabor.ii291l53/{z}/{x}/{y}.png', {
                 maxZoom: 17,
             }).addTo(map);
 
             var marker;
-
             var markers = new Array();
 
             var greenIcon = L.icon({
@@ -49,9 +48,19 @@ function($, _, Parse, Handlebars, template, popupTemplate,Warrior, Weapon, Head,
                 iconAnchor:   [19, 44], // point of the icon which will correspond to marker's location
             });
 
+            var circle = undefined;
+            var radius = JSON.parse(window.localStorage.getItem("weapon")).range + JSON.parse(window.localStorage.getItem("head")).range;
+
             navigator.geolocation.getCurrentPosition(function (position) {
                 var coord = new L.LatLng(position.coords.latitude, position.coords.longitude);
                 marker = L.marker([position.coords.latitude, position.coords.longitude], {icon: greenIcon}).addTo(map);
+                circle = new L.circle([position.coords.latitude, position.coords.longitude], radius, {
+                    color: '#63ca3a',
+                    fillColor: '#63ca3a',
+                    fillOpacity: 0.3,
+                    weight: 2,
+                    clickable: false
+                }).addTo(map);
             }, function() {});
 
             function mapUpdate(position){
@@ -61,6 +70,8 @@ function($, _, Parse, Handlebars, template, popupTemplate,Warrior, Weapon, Head,
                     marker.setLatLng([position.coords.latitude, position.coords.longitude]).update();
                     self.model.position.latitude=position.coords.latitude;
                     self.model.position.longitude=position.coords.longitude;
+
+                    circle.setLatLng([position.coords.latitude, position.coords.longitude]);
                     // aggiorniamo nostra posizione su parse
                     $.ajax({
                         type : 'PUT',
@@ -94,69 +105,85 @@ function($, _, Parse, Handlebars, template, popupTemplate,Warrior, Weapon, Head,
 
                                 if (markers[self.warriors[i].id] == undefined){
                                     markers[self.warriors[i].id] = new L.marker([self.warriors[i].get("position").latitude, self.warriors[i].get("position").longitude], 
-                                                                            {icon: redIcon})
+                                                                            {icon: redIcon});
                                     markers[self.warriors[i].id].addTo(map); 
                                 }
                                 else {
+                                    var a = new L.LatLng(position.coords.latitude, position.coords.longitude);
                                     var b = new L.LatLng(self.warriors[i].get("position").latitude, self.warriors[i].get("position").longitude);
-                                    
-                                    
-                                    markers[self.warriors[i].id].setLatLng(b).update();
-                                    markers[self.warriors[i].id].off('mousedown');
-                                    markers[self.warriors[i].id].on('mousedown', function(e){
-                                        var mark = e.target;
-                                        for(var y = 1; y<self.warriors.length; y++){
-                                            var locWar = new L.LatLng(self.warriors[y].get("position").latitude, self.warriors[y].get("position").longitude);
-                                            if (mark.getLatLng().equals(locWar)){
-                                                
-                                                var enemyWar = self.warriors[y];
 
-
-                                                var weaponClass = Parse.Object.extend("Weapon");
-                                                var headClass = Parse.Object.extend("Head");
-                                                var armorClass = Parse.Object.extend("Armor");
-                                               
-                                                var queryWeapon = new Parse.Query(weaponClass);
-                                                var queryHead = new Parse.Query(headClass);
-                                                var queryArmor = new Parse.Query(armorClass);
-
-                                                queryWeapon.get(enemyWar.get("weapon").id, {
-                                                    success: function(weapon) {
-                                                       window.localStorage.setItem("enemyWeapon",JSON.stringify(weapon));
-                                                    }
-                                                });
-
-                                                queryHead.get(enemyWar.get("head").id, {
-                                                    success: function(head) {
-                                                       window.localStorage.setItem("enemyHead",JSON.stringify(head));
-                                                    }
-                                                });
-
-                                                queryArmor.get(enemyWar.get("armor").id, {
-                                                    success: function(armor) {
-                                                       window.localStorage.setItem("enemyArmor",JSON.stringify(armor));
-                                                    }
-                                                });
-
-                                                $('#fight').off('mousedown');
-                                                $('#fight').on("mousedown", function() {
-
-                                                    var enemyWeapon = JSON.parse(window.localStorage.getItem("enemyWeapon"));
-                                                    var enemyHead = JSON.parse(window.localStorage.getItem("enemyHead"));
-                                                    var enemyArmor = JSON.parse(window.localStorage.getItem("enemyArmor"));
-
-                                                    var myWeapon = JSON.parse(window.localStorage.getItem("weapon"));
-                                                    var myHead = JSON.parse(window.localStorage.getItem("head"));
-                                                    var myArmor = JSON.parse(window.localStorage.getItem("armor"));
-                                                    
-                                                    fight(enemyWar, enemyWeapon, enemyHead, enemyArmor, myWeapon, myHead, myArmor);
-                                                });
-                                                $('#popupWarrior').show(100);
-                                                $('#infoWarrior').empty();
-                                                $('#infoWarrior').append("<p>"+self.warriors[y].get("nick")+"</p><p>Level: "+self.warriors[y].get("level")+"</p>");
-                                            }
-                                        }
+                                    var warPoint = new Parse.GeoPoint({
+                                        latitude: self.warriors[i].get("position").latitude,
+                                        longitude: self.warriors[i].get("position").longitude
                                     });
+                                    
+                                    var distance = a.distanceTo(b);
+                                    
+                                    if(distance>radius){
+                                        
+                                        markers[self.warriors[i].id].setLatLng(b).update();
+                                        markers[self.warriors[i].id].off('mousedown');
+                                    }
+                                    
+                                    else{
+        
+                                        markers[self.warriors[i].id].setLatLng(b).update();
+                                        markers[self.warriors[i].id].off('mousedown');
+                                        markers[self.warriors[i].id].on('mousedown', function(e){
+                                            var mark = e.target;
+                                            for(var y = 1; y<self.warriors.length; y++){
+                                                var locWar = new L.LatLng(self.warriors[y].get("position").latitude, self.warriors[y].get("position").longitude);
+                                                if (mark.getLatLng().equals(locWar)){
+                                                    
+                                                    var enemyWar = self.warriors[y];
+
+
+                                                    var weaponClass = Parse.Object.extend("Weapon");
+                                                    var headClass = Parse.Object.extend("Head");
+                                                    var armorClass = Parse.Object.extend("Armor");
+                                                   
+                                                    var queryWeapon = new Parse.Query(weaponClass);
+                                                    var queryHead = new Parse.Query(headClass);
+                                                    var queryArmor = new Parse.Query(armorClass);
+
+                                                    queryWeapon.get(enemyWar.get("weapon").id, {
+                                                        success: function(weapon) {
+                                                           window.localStorage.setItem("enemyWeapon",JSON.stringify(weapon));
+                                                        }
+                                                    });
+
+                                                    queryHead.get(enemyWar.get("head").id, {
+                                                        success: function(head) {
+                                                           window.localStorage.setItem("enemyHead",JSON.stringify(head));
+                                                        }
+                                                    });
+
+                                                    queryArmor.get(enemyWar.get("armor").id, {
+                                                        success: function(armor) {
+                                                           window.localStorage.setItem("enemyArmor",JSON.stringify(armor));
+                                                        }
+                                                    });
+
+                                                    $('#fight').off('mousedown');
+                                                    $('#fight').on("mousedown", function() {
+
+                                                        var enemyWeapon = JSON.parse(window.localStorage.getItem("enemyWeapon"));
+                                                        var enemyHead = JSON.parse(window.localStorage.getItem("enemyHead"));
+                                                        var enemyArmor = JSON.parse(window.localStorage.getItem("enemyArmor"));
+
+                                                        var myWeapon = JSON.parse(window.localStorage.getItem("weapon"));
+                                                        var myHead = JSON.parse(window.localStorage.getItem("head"));
+                                                        var myArmor = JSON.parse(window.localStorage.getItem("armor"));
+                                                        
+                                                        fight(enemyWar, enemyWeapon, enemyHead, enemyArmor, myWeapon, myHead, myArmor);
+                                                    });
+                                                    $('#popupWarrior').show(100);
+                                                    $('#infoWarrior').empty();
+                                                    $('#infoWarrior').append("<p>"+self.warriors[y].get("nick")+"</p><p>Level: "+self.warriors[y].get("level")+"</p>");
+                                                }
+                                            }
+                                        });
+                                    }
                                 } 
                             }
                         }
