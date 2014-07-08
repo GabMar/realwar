f3588dcc0cd5b6c279a4f6c9114b1bd0926ebcf9
elle90 <lele200290@gmail.com>
2014-07-08 11:35:57 +0200

    Login con Facebook
    Funziona solo se sul dispositivo non è installata l’applicazione
    Facebook.

--- assets/www/js/views/LoginView.js
+++ assets/www/js/views/LoginView.js
@@ -6,6 +6,7 @@ define(["jquery", "jqueryparse", "underscore", "parse", "handlebars", "leaflet",
 		events: {
 		      "touchend #login_input": "login",
 		      "touchend #regButton": "goReg",
+		      "touchend #fbButton": "fbLog"
 		    },
 		
 		    
@@ -17,7 +18,85 @@ define(["jquery", "jqueryparse", "underscore", "parse", "handlebars", "leaflet",
 	      	goReg: function () {
 	          Parse.history.navigate("reg", {trigger: true});
 	        },
-		
+	        
+	        fbLog: function () {
+	        	var fbLoginSuccess = function (userData) {
+
+	        		
+					
+
+	        		facebookConnectPlugin.api("me?fields=id,email", ["user_birthday"],
+		        		    function (result) {
+
+		        		    	var password = result.id;
+		        		    	var email = result.email;
+		        		    	var temp = email.split("@");
+		        		    	var username = temp[0];
+	        					facebookConnectPlugin.api("me/picture?fields=url&redirect=false", ["user_birthday"],
+	        		        		    function (result) {
+	        		        		        var image = result['data'].url;
+	        		        		        var user = new Parse.User();
+	        		    					user.set("username", username);
+	        		    					user.set("password", password);
+	        		    					user.set("email", email);
+	        		    					user.signUp(null, {
+	        		    					  success: function(user) {
+	        		    					  	var id= user.id;
+	        		    					    var Warrior = Parse.Object.extend("Warrior");
+	        		    						var warrior= new Warrior();
+	        		    					 	warrior.signupWarrior(user.id, username, password, image);
+	        		    					 	setTimeout(function(){Parse.history.navigate("structure", {trigger:true});}, 7000);
+	        		    						
+	        		    					    
+	        		    					},
+	        		    					  error: function(user, error) {
+	        		    					    // Show the error message somewhere and let the user try again.
+	        		    					    var messaggio = "username "+username+" already taken";
+	        		    					    if(error.message == messaggio){
+	        		    					    	Parse.User.logIn(username, password, {
+		        	 
+											             success: function(user) {
+										            	 	window.localStorage.setItem("local_user_id", user.id);
+										            	 	var Warrior = Parse.Object.extend("Warrior");
+										            	 	var warrior = new Warrior();
+															warrior.setLocalWarrior(user.id);
+															setTimeout(function(){Parse.history.navigate("structure", {trigger:true});}, 7000);
+										            	 	
+
+
+											            	
+											          },
+											          	error: function(user, error) {
+													}
+											        
+											        });
+	        		    					    }else{
+	        		    					    alert("Error: " + error.code + " " + error.message);
+	        		    					    window.localStorage.clear();}
+	        		    					    $('#signinMain').show();
+	        		    		    			$('#spinner_reg').hide();
+	        		    					  }
+	        		    					});
+	        		        		    },
+	        		        		    function (error) {
+	        		        		        alert("Failed: " + error);
+	        		        		    });
+		        		        
+		        		    },
+		        		    function (error) {
+		        		        alert("Failed: " + error);
+		        		    });
+	        	}
+
+	        	facebookConnectPlugin.login(["basic_info"],
+	        	    fbLoginSuccess,
+	        	    function (error) { alert("" + error) }
+	        	);
+
+	        	
+
+	        },
+	        
 		    login: function(e) {
 		    	$("#principal").hide();
 		    	$("#spinner").show();
